24bc2d7508f703a957ebb8647bbd3c0c76da2ffc
giladd <giladd@trainologic.com>
2019-04-23 17:59:12 +0300

    format

--- src/loopify.js
+++ src/loopify.js
@@ -1,94 +1,89 @@
 (function() {
+  function loopify(uri, cb) {
+    var context = new (window.AudioContext || window.webkitAudioContext)(),
+      request = new XMLHttpRequest();
 
-    function loopify(uri,cb) {
+    request.responseType = "arraybuffer";
+    request.open("GET", uri, true);
 
-      var context = new (window.AudioContext || window.webkitAudioContext)(),
-          request = new XMLHttpRequest();
+    // XHR failed
+    request.onerror = function() {
+      cb(new Error("Couldn't load audio from " + uri));
+    };
 
-      request.responseType = "arraybuffer";
-      request.open("GET", uri, true);
+    // XHR complete
+    request.onload = function() {
+      context.decodeAudioData(request.response, success, function(err) {
+        // Audio was bad
+        cb(new Error("Couldn't decode audio from " + uri));
+      });
+    };
 
-      // XHR failed
-      request.onerror = function() {
-        cb(new Error("Couldn't load audio from " + uri));
-      };
+    request.send();
 
-      // XHR complete
-      request.onload = function() {
-        context.decodeAudioData(request.response,success,function(err){
-          // Audio was bad
-          cb(new Error("Couldn't decode audio from " + uri));
-        });
-      };
+    function success(buffer) {
+      var source;
+      var started;
 
-      request.send();
+      function play() {
+        // console.log('loopify start');
+        // Stop if it's already playing
+        stopImmediate();
 
-      function success(buffer) {
+        // Create a new source (can't replay an existing source)
+        source = context.createBufferSource();
+        source.connect(context.destination);
 
-        var source;
-        var started;
+        // Set the buffer
+        source.buffer = buffer;
+        source.loop = true;
 
-        function play() {
-          // console.log('loopify start');
-          // Stop if it's already playing
-          stopImmediate();
-
-          // Create a new source (can't replay an existing source)
-          source = context.createBufferSource();
-          source.connect(context.destination);
-
-          // Set the buffer
-          source.buffer = buffer;
-          source.loop = true;
-
-          // Play it
-          started = context.currentTime;
-          source.start(0);
-
-        }
-
-        function stopImmediate() {
-          // console.log('loopify stop immediate', !!source);
-          // Stop and clear if it's playing
-          if (source) {
-              source.stop();
-              source = null;
-          }
-        }
-
-        function stop() {
-          // console.log('loopify stop', !!source);
-          // Stop and clear if it's playing
-          if (source) {
-            var elapsed = context.currentTime - started;
-            var timeLeft = source.buffer.duration - elapsed % source.buffer.duration;
-            // console.log('elapsed', elapsed, 'left', timeLeft)
-            setTimeout(function() {
-              // console.log('loopify stop timeout', !!source);
-              source.stop();
-              source = null;
-            }, timeLeft * 1000);
-          }
-
-        }
-
-        cb(null,{
-          play: play,
-          stop: stop
-        });
-
+        // Play it
+        started = context.currentTime;
+        source.start(0);
       }
 
+      function stopImmediate() {
+        // console.log('loopify stop immediate', !!source);
+        // Stop and clear if it's playing
+        if (source) {
+          source.stop();
+          source = null;
+        }
+      }
+
+      function stop() {
+        // console.log('loopify stop', !!source);
+        // Stop and clear if it's playing
+        if (source) {
+          var elapsed = context.currentTime - started;
+          var timeLeft =
+            source.buffer.duration - (elapsed % source.buffer.duration);
+          // console.log('elapsed', elapsed, 'left', timeLeft)
+          setTimeout(function() {
+            // console.log('loopify stop timeout', !!source);
+            source.stop();
+            source = null;
+          }, timeLeft * 1000);
+        }
+      }
+
+      cb(null, {
+        play: play,
+        stop: stop
+      });
     }
+  }
 
-    loopify.version = "0.1";
+  loopify.version = "0.1";
 
-    if (typeof define === "function" && define.amd) {
-      define(function() { return loopify; });
-    } else if (typeof module === "object" && module.exports) {
-      module.exports = loopify;
-    } else {
-      this.loopify = loopify;
-    }
-
+  if (typeof define === "function" && define.amd) {
+    define(function() {
+      return loopify;
+    });
+  } else if (typeof module === "object" && module.exports) {
+    module.exports = loopify;
+  } else {
+    this.loopify = loopify;
+  }
 })();
